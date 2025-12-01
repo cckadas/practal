8584b4ad6071dfe72303defca1325d932368b8d9
olv <olvjar@gmail.com>
2025-12-01 23:43:22 +0800

    feat: expand on post system, add support for regular and practice post types

--- app/src/main/java/com/itismob/s15/group7/practal/domain/model/Post.kt
+++ app/src/main/java/com/itismob/s15/group7/practal/domain/model/Post.kt
@@ -2,13 +2,48 @@ package com.itismob.s15.group7.practal.domain.model
 
 import com.google.firebase.Timestamp
 
-data class Post(
-    val id: String = "",
-    val owner_email: String = "",
-    val owner_img: String = "",
-    val post_img: String = "",
-    val caption: String = "",
-    val likes: List<String> = emptyList(),
-    val comments: List<String> = emptyList(),
-    val date_posted: Timestamp = Timestamp.now()
-)
+sealed class Post {
+    abstract val id: String
+    abstract val owner_email: String
+    abstract val owner_img: String
+    abstract val caption: String
+    abstract val likes: List<String>
+    abstract val comments: List<String>
+    abstract val date_posted: Timestamp
+    abstract val postType: String
+    
+    // regular post
+    data class RegularPost(
+        override val id: String = "",
+        override val owner_email: String = "",
+        override val owner_img: String = "",
+        val post_img: String = "", // optional
+        override val caption: String = "",
+        override val likes: List<String> = emptyList(),
+        override val comments: List<String> = emptyList(),
+        override val date_posted: Timestamp = Timestamp.now(),
+        override val postType: String = "regular"
+    ) : Post()
+    
+    // practice session
+    data class PracticePost(
+        override val id: String = "",
+        override val owner_email: String = "",
+        override val owner_img: String = "",
+        override val caption: String = "",
+        val post_img: String = "", // optional image
+        override val likes: List<String> = emptyList(),
+        override val comments: List<String> = emptyList(),
+        override val date_posted: Timestamp = Timestamp.now(),
+        override val postType: String = "practice",
+        
+        // practice session details
+        val sessionId: String = "", //  practice session
+        val instrument: String = "",
+        val practiceType: String = "",
+        val pieceOrFocus: String = "",
+        val durationMinutes: Int = 0,
+        val difficulty: String = "",
+        val earnedXP: Int = 0
+    ) : Post()
+}
